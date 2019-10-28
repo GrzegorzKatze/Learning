a0ac4bd5bb1d4a9a5aef3d1f30e49ad914760edf
grzegorzkatze <grzegorzkatze@o2.pl>
2019-10-28 14:00:17 +0100

    Initial Commit

---
+++ AngularHeroesTutorial/src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './heroes/hero';
+
+export const HEROES: Hero[] = [
+    { id: 1, name: 'Iron-Man'},
+    { id: 2, name: 'Cpt. America'},
+    { id: 3, name: 'Thor'},
+    { id: 4, name: 'Spider-Man'},
+    { id: 5, name: 'Batman'}
+];
